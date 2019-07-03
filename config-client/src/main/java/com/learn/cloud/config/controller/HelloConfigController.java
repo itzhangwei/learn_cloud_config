4ea7d084c8eeca2cfadfcb59ed6617bef40974e4
zack.zhang <zhangwei>
2019-07-03 16:50:56 +0800

    actuator，监控功能，刷新配置

--- config-client/src/main/java/com/learn/cloud/config/controller/HelloConfigController.java
+++ config-client/src/main/java/com/learn/cloud/config/controller/HelloConfigController.java
@@ -1,6 +1,7 @@
 package com.learn.cloud.config.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2019-07-03 10:38
  */
 @RestController
+@RefreshScope // 当配置中心配置文件修改后，接收到SpringCloud配置中心刷新的时候，自动将配置中心刷新到对应属性上
 public class HelloConfigController {
     @Value(value = "${my.motto}")
     private String motto;
